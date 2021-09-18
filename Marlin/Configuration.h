9842e94d4f0707f58b7a1297b9e8f729aede993a
Alexandra 'Lexa' Baldwin <lexa.b@outlook.com>
2021-09-18 11:25:19 -0700

    increased Marlin bed size to make the whole build area usable

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1359,8 +1359,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 110
-#define Y_BED_SIZE 110
+#define X_BED_SIZE 150
+#define Y_BED_SIZE 170
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
